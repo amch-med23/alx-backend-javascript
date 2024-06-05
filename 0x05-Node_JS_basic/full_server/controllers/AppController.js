3afa6a6c33423437df926d51f6ff30e365cfdf3c
amch-med23 <skyied.med2023@gmail.com>
2024-06-05 03:26:03 +0200

    creating the appController class

--- 0x05-Node_JS_basic/full_server/controllers/AppController.js
+++ 0x05-Node_JS_basic/full_server/controllers/AppController.js
@@ -1,1 +1,8 @@
-tr
+class AppController {
+  static getHomepage(request, response) {
+    response.status(200);
+    response.send('Hello Holberton School!');
+  }
+}
+
+module.exports = AppController;
