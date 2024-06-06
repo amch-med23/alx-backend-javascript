51d96005690f7c23fb35b611b09fae7a9bde0c83
amch-med23 <skyied.med2023@gmail.com>
2024-06-07 00:46:31 +0200

    defining the getPaymentTokenFromApi function

--- 0x06-unittests_in_js/6-payment_token.js
+++ 0x06-unittests_in_js/6-payment_token.js
@@ -1,1 +1,7 @@
-ts
+const getPaymentTokenFromAPI = (success) => {
+  if (success) return new Promise((resolve, reject) => {
+    resolve({ data: 'Successful response from the API' });
+  });
+};
+
+module.exports = getPaymentTokenFromAPI;
