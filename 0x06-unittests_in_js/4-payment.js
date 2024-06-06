8e128782b3b4dea352e2b675380a69da18e684e2
amch-med23 <skyied.med2023@gmail.com>
2024-06-07 00:41:06 +0200

    definging the paymeny file using chai

--- 0x06-unittests_in_js/4-payment.js
+++ 0x06-unittests_in_js/4-payment.js
@@ -1,1 +1,10 @@
-ts
+const { util } = require("chai");
+const Utils = require("./utils.js");
+
+const sendPaymentRequestToApi = (totalAmount, totalShipping) => {
+  const res = Utils.calculateNumber('SUM', totalAmount, totalShipping);
+  console.log(`The total is: ${res}`);
+  return res;
+};
+
+module.exports = sendPaymentRequestToApi
